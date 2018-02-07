da87d1f4620c2e33224f4facb81a1b9e238d334b
Grzegorz Ziemonski <grzegorz.ziemonski@gmail.com>
2018-02-07 22:59:58 +0100

    Add colors to printed messages.

--- den.sh
+++ den.sh
@@ -4,24 +4,47 @@ set -e
 
 install() {
     if [ -f "/usr/bin/den" ]; then
-        echo "Already installed."
+        printf "\033[0;33mAlready installed.\033[0m\n"
         exit
     fi
 
     if [ ! -f den.sh ]; then
-        echo "You must run the installation from den's source directory."
+        printf "\033[0;31mYou must run the installation from den's source directory.\033[0m\n"
         exit 1
     fi
 
     dirPart="$(pwd)"
     ln -s "$dirPart/den.sh" /usr/bin/den
 
-    echo "Installed."
+    printf "\033[0;32mInstalled.\033[0m\n"
+}
+
+remove() {
+    if [ ! -f "/usr/bin/den" ]; then
+        printf "\033[0;33mNot installed.\033[0m\n"
+        exit
+    fi
+
+    rm /usr/bin/den
+    printf "\033[0;32mRemoved.\033[0m\n"
 }
 
 launch() {
     toRun=$1
     toPush=$2
+
+    containerName=den_$(echo "$toRun" | tr / _)
+
+    container=$(docker ps -a -q --filter=name=$containerName)
+
+    if [ -n "$container" ]; then
+        printf "\
+\033[0;31mERROR! Environment container exists!
+It's possible that den has not exited properly the last time it was ran.
+You need to commit and push your progress (if needed) and remove the container manually.
+\033[0m"
+        exit 1
+    fi
 
     mkdir -p /tmp/den
 
@@ -29,25 +52,24 @@ launch() {
     
     echo "
         xhost +local:
-        docker run -e DISPLAY=\$DISPLAY \\
+        docker run --name $containerName \\
+                   -e DISPLAY=\$DISPLAY \\
                    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \\
                    $DOCKER_OPTS \\
                    $toRun > /tmp/den/container.log
     " > /tmp/den/xinitrc
 
     startx /tmp/den/xinitrc
-      
-    image=$(docker ps -a -q --filter=ancestor=$toRun)
   
-    docker commit $image $toPush
+    docker commit $containerName $toPush
     docker push $toPush
-    docker rm $image
+    docker rm $containerName
 }
 
 if [ "$1"  == "install" ]; then
     install
 elif [ "$1"  == "remove" ]; then
-    rm /usr/bin/den
+    remove
 elif [ "$1"  == "init" ]; then
     echo "Initializing environment $3 using $2..."
     launch $2 $3
